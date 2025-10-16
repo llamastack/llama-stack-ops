8f78d31c07ade27e85238b4b84626c6db6009205
slekkala1 <swapna942@meta.com>
2025-10-16 10:48:45 -0700

    remove unwanted env variable (#16)

--- actions/test-and-cut/main.sh
+++ actions/test-and-cut/main.sh
@@ -135,7 +135,6 @@ test_docker() {
     -e OLLAMA_URL=http://localhost:11434 \
     -e SAFETY_MODEL=ollama/llama-guard3:1b \
     -e LLAMA_STACK_TEST_INFERENCE_MODE=replay \
-    -e LLAMA_STACK_TEST_RECORDING_DIR=/app/llama-stack-source/tests/integration/recordings \
     -e LLAMA_STACK_TEST_STACK_CONFIG_TYPE=server \
     -e TOGETHER_API_KEY=$TOGETHER_API_KEY \
     -e FIREWORKS_API_KEY=$FIREWORKS_API_KEY \
