fe98d64438d9fa485cd97694a9e2b507ac7599d2
raghotham <rsm@meta.com>
2025-12-02 16:22:45 -0600

    chore: add copyright header and license to readme (#34)

--- actions/upload-packages-and-tag/main.sh
+++ actions/upload-packages-and-tag/main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
+
 if [ -z "$VERSION" ]; then
   echo "You must set the VERSION environment variable" >&2
   exit 1
